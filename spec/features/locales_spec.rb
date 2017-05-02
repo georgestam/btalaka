c04bb3eb8d441d89c6ba4dc2149a1472b187212c
Jordi Fernandez <jordi@ywarp.com>
2017-05-02 14:09:13 +0100

    Adapt locale_spec.rb with before method to pass test

--- spec/features/locales_spec.rb
+++ spec/features/locales_spec.rb
@@ -1,8 +1,10 @@
 describe 'Locale switching when landing page is in English' do
   
-  visit root_path(:en)
+  before {
+    visit root_path(:en)
+  }
     
-  it 'changes to locale in arabic when arabic button is selected' do
+  it 'changes to locale in Arabic when Arabic button is clicked' do
     expect{ find("#change-to-arabic").click }.to change{ I18n.locale }.from(:en).to(:ea)
     page_ok
   end
