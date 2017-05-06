f4b80fd640dfb7978b7faf55b11bf8b72a7cb984
Jordi Fernandez <jordi@ywarp.com>
2017-05-06 20:59:42 +0100

    Correct code to make rubocop pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  skip_before_filter  :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
   
   before_action :set_locale
   
