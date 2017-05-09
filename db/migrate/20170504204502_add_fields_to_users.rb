cfd1abd40d078dd864e29b18992a4204e9f406a0
Jordi Fernandez <neowax@Jordi-Fernandezs-MacBook-Pro.local>
2017-05-09 11:48:56 +0100

    change mobile from integer to string

--- db/migrate/20170504204502_add_fields_to_users.rb
+++ db/migrate/20170504204502_add_fields_to_users.rb
@@ -1,7 +1,7 @@
 class AddFieldsToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :name, :string
-    add_column :users, :mobile, :integer
+    add_column :users, :mobile, :string
   end
 end
 
