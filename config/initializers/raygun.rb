afca686f4748972d25cc3514cf4176601ca543f2
Jordi Fernandez <jordi@ywarp.com>
2017-06-23 09:19:17 +0100

    Modify raygun settings

--- config/initializers/raygun.rb
+++ config/initializers/raygun.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
-
 Raygun.setup do |config|
   config.api_key = ENV['RAYGUN_APIKEY']
   config.filter_parameters = Rails.application.config.filter_parameters
 
-  config.enable_reporting = production_or_staging?
+  # config.enable_reporting = production_or_staging?
 end
